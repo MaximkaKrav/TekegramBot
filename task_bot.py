d04e6e6e9b940e400d24a3dc5f371f8bd9467298
maksimkravcov <isyamises@gmail.com>
2023-12-21 15:50:50 +0300

    Переделал внешний вид клавиш взаимодействия Увеличил читаемость кода

--- task_bot.py
+++ task_bot.py
@@ -10,6 +10,35 @@ bot = telebot.TeleBot(TOKEN)
 tasks = {}  # Dictionary to store tasks for each user
 user_states = {}  # Dictionary to track user states
 
+def send_welcome_message(message):
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    add_task_button = types.InlineKeyboardButton('➕ Добавить задачу', callback_data='add_task')
+    show_tasks_button = types.InlineKeyboardButton('📋 Показать все задачи', callback_data='show_tasks')
+    markup.add(add_task_button, show_tasks_button)
+
+    bot.send_message(message.chat.id, "👋 Привет! Я твой менеджер задач. Что будем делать?", reply_markup=markup)
+    user_states[message.from_user.id]['first_start'] = False
+
+def update_keyboard(message):
+    user_id = message.from_user.id
+    markup = types.InlineKeyboardMarkup(row_width=2)
+
+    add_task_button = types.InlineKeyboardButton('➕ Добавить задачу', callback_data='add_task')
+    markup.add(add_task_button)
+
+    show_tasks_button = types.InlineKeyboardButton('📋 Показать все задачи', callback_data='show_tasks')
+    markup.add(show_tasks_button)
+
+    # Проверяем наличие задач для данного пользователя
+    if user_id in tasks and tasks[user_id]:
+        delete_task_button = types.InlineKeyboardButton('🗑️ Удалить запись', callback_data='delete_task')
+        markup.add(delete_task_button)
+
+        delete_all_button = types.InlineKeyboardButton('🗑️ Удалить все записи', callback_data='delete_all')
+        markup.add(delete_all_button)
+
+    bot.send_message(message.chat.id, "Что дальше?", reply_markup=markup)
+
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     user_id = message.from_user.id
@@ -19,37 +48,30 @@ def handle_start(message):
     else:
         update_keyboard(message)
 
-def send_welcome_message(message):
-    markup = types.ReplyKeyboardMarkup(row_width=2)
-    add_task_button = types.KeyboardButton('Добавить задачу')
-    show_tasks_button = types.KeyboardButton('Показать все задачи')
-    markup.add(add_task_button, show_tasks_button)
+@bot.callback_query_handler(func=lambda call: True)
+def handle_callback_query(call):
+    message = call.message
+    user_id = call.from_user.id
 
-    bot.send_message(message.chat.id, "👋 Привет! Я твой менеджер задач. Что будем делать?", reply_markup=markup)
-    user_states[message.from_user.id]['first_start'] = False
+    if call.data == 'add_task':
+        bot.send_message(message.chat.id, "📝 Введи название задачи:")
+        bot.register_next_step_handler(message, add_task)
+    elif call.data == 'show_tasks':
+        if user_id in tasks and tasks[user_id]:
+            task_list = '\n'.join(tasks[user_id])
+            bot.send_message(message.chat.id, f'📋 Твои задачи:\n{task_list}')
+        else:
+            bot.send_message(message.chat.id, '🤔 У тебя нет задач.')
+    elif call.data == 'delete_task':
+        bot.send_message(message.chat.id, "🗑️ Выбери какую запись ты хочешь удалить (введи номер или полное название):")
+        bot.register_next_step_handler(message, delete_task)
+    elif call.data == 'delete_all':
+        if user_id in tasks:
+            del tasks[user_id]  # Удаляем все задачи пользователя
+        bot.send_message(message.chat.id, '🗑️ Все записи успешно удалены.')
 
-def update_keyboard(message):
-    user_id = message.from_user.id
-    markup = types.ReplyKeyboardMarkup(row_width=2)
-    add_task_button = types.KeyboardButton('Добавить задачу')
-    show_tasks_button = types.KeyboardButton('Показать все задачи')
-
-    # Проверяем наличие задач для данного пользователя
-    if user_id in tasks and tasks[user_id]:
-        delete_task_button = types.KeyboardButton('Удалить запись')
-        delete_all_button = types.KeyboardButton('Удалить все записи')
-        markup.add(add_task_button, show_tasks_button, delete_task_button, delete_all_button)
-    else:
-        markup.add(add_task_button, show_tasks_button)
-
-    bot.send_message(message.chat.id, "Что дальше?", reply_markup=markup)
-
-
-@bot.message_handler(func=lambda message: message.text == 'Добавить задачу')
-def handle_add_task(message):
-    bot.send_message(message.chat.id, "📝 Введи название задачи:")
-    bot.register_next_step_handler(message, add_task)
-
+        # Обновление клавиатуры после удаления всех задач
+        update_keyboard(message)
 
 def add_task(message):
     task_name = message.text
@@ -64,27 +86,6 @@ def add_task(message):
 
     # Обновление клавиатуры после добавления задачи
     update_keyboard(message)
-
-
-@bot.message_handler(func=lambda message: message.text == 'Показать все задачи')
-def handle_show_tasks(message):
-    user_id = message.from_user.id
-
-    if user_id in tasks and tasks[user_id]:
-        task_list = '\n'.join(tasks[user_id])
-        bot.send_message(message.chat.id, f'📋 Твои задачи:\n{task_list}')
-    else:
-        bot.send_message(message.chat.id, '🤔 У тебя нет задач.')
-
-    # Обновление клавиатуры после просмотра задач
-    update_keyboard(message)
-
-
-@bot.message_handler(func=lambda message: message.text == 'Удалить запись')
-def handle_delete_task(message):
-    bot.send_message(message.chat.id, "🗑️ Выбери какую запись ты хочешь удалить (введи номер или полное название):")
-    bot.register_next_step_handler(message, delete_task)
-
 
 def delete_task(message):
     user_id = message.from_user.id
@@ -112,20 +113,6 @@ def delete_task(message):
 
     # Обновление клавиатуры после удаления задачи
     update_keyboard(message)
-
-
-@bot.message_handler(func=lambda message: message.text == 'Удалить все записи')
-def handle_delete_all(message):
-    user_id = message.from_user.id
-
-    if user_id in tasks:
-        del tasks[user_id]  # Удаляем все задачи пользователя
-
-    bot.send_message(message.chat.id, '🗑️ Все записи успешно удалены.')
-
-    # Обновление клавиатуры после удаления всех задач
-    update_keyboard(message)
-
 
 # Обработка выхода из бота вручную (если бот прерван)
 try:
